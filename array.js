4a4b739b8aa1cfd60ee8dead015ed05499c05c8c
anishchavan <anishchavan29@gmail.com>
2023-05-13 23:58:07 +0530

    another way of declaring array but not recommended

--- array.js
+++ array.js
@@ -1,11 +1,16 @@
 // Array 
 // Array methods
 
-let names=['anish','suhas','ganesh','shyam','mukesh','roshani','seema']
+// let names=['anish','suhas','ganesh','shyam','mukesh','roshani','seema']
 
-console.log(names)
-console.log(names.length)
-console.log(names[3])
-console.log(names[names.length-1])
-names[6]='makram'
+// console.log(names)
+// console.log(names.length)
+// console.log(names[3])
+// console.log(names[names.length-1])
+// names[6]='makram'
+// console.log(names)
+
+// another way of declaring array
+
+let names = new Array('nilesh', 'manish', 'santosh')
 console.log(names)
