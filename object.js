6f5bc9873959c8d6556cd5032612b2e01932866b
anishchavan <anishchavan29@gmail.com>
2023-05-14 23:55:11 +0530

    object

---
+++ object.js
@@ -0,0 +1,17 @@
+// object
+
+let userName = {
+  firstName: "Anish",
+  lastName: "Chavan",
+  mobile: 8600236197,
+};
+
+// console.log(userName)
+// console.log(userName.lastName)
+// userName.mobile = 9049961023
+// console.log(userName)
+
+// for in
+for (let x in userName) {
+    console.log(userName[x])
+}
