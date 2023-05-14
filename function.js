18e310135af04c92ffab589bde2afcd76384e5b8
anishchavan <anishchavan29@gmail.com>
2023-05-14 23:30:14 +0530

    functions and uses

--- function.js
+++ function.js
@@ -1,104 +1,104 @@
-// functions
+functions
 
-// function sum() {
-//     let num1 = 12
-//     let num2 = 2
-//     console.log('The sum of two numbers are', num1+num2)
-// }
-// sum()
-// sum()
-// sum()
-// sum()
+function sum() {
+    let num1 = 12
+    let num2 = 2
+    console.log('The sum of two numbers are', num1+num2)
+}
+sum()
+sum()
+sum()
+sum()
 
-// function sum(num1,num2) {
-//   console.log("The sum of two numbers are", num1 + num2);
-// }
-// sum(2,3);
-// sum(4,5);
-// sum(6,7);
-// sum(8,9);
+function sum(num1,num2) {
+  console.log("The sum of two numbers are", num1 + num2);
+}
+sum(2,3);
+sum(4,5);
+sum(6,7);
+sum(8,9);
 
-// funtion  with return keyword
-// function sum(val1, val2){
-//     let result = val1 + val2
-//     return result
-// }
-// let add=sum(2,3)
-// console.log(add)
+funtion  with return keyword
+function sum(val1, val2){
+    let result = val1 + val2
+    return result
+}
+let add=sum(2,3)
+console.log(add)
 
-// function sum(val1, val2){
-//     let result = val1 - val2
-//     return result
-// }
-// let add=sum(2,3)
-// console.log(add)
+function sum(val1, val2){
+    let result = val1 - val2
+    return result
+}
+let add=sum(2,3)
+console.log(add)
 
-// function sum(val1, val2){
-//     let result = val1 * val2
-//     return result
-// }
-// let add=sum(2,3)
-// console.log(add)
+function sum(val1, val2){
+    let result = val1 * val2
+    return result
+}
+let add=sum(2,3)
+console.log(add)
 
-// function sum(val1, val2){
-//     let result = val1 / val2
-//     return result
-// }
-// let add=sum(2,3)
-// console.log(add)
+function sum(val1, val2){
+    let result = val1 / val2
+    return result
+}
+let add=sum(2,3)
+console.log(add)
 
-// function sum(val1, val2){
-//     let result = val1 % val2
-//     return result
-// }
-// let add=sum(2,3)
-// console.log(add)
+function sum(val1, val2){
+    let result = val1 % val2
+    return result
+}
+let add=sum(2,3)
+console.log(add)
 
-// function fullName(middleName, surName){
-//     let firstName = 'Anish'
-//     let result= firstName+middleName+surName
-//     return result;
-// }
+function fullName(middleName, surName){
+    let firstName = 'Anish'
+    let result= firstName+middleName+surName
+    return result;
+}
 
-// let myName = fullName(' Ramcharan',' Chavan')
-// console.log(myName)
+let myName = fullName(' Ramcharan',' Chavan')
+console.log(myName)
 
-// function fullName(middleName, surName){
-//     let firstName = 'Anish'
-//     let result = firstName+middleName+surName
-//     console.log(result)
-// }
+function fullName(middleName, surName){
+    let firstName = 'Anish'
+    let result = firstName+middleName+surName
+    console.log(result)
+}
 
-// fullName(' Ramcharan', ' Chavan')
+fullName(' Ramcharan', ' Chavan')
 
-// function sum(array) {
-//   let sum = 0;
-//   for (let index = 0; index < array.length; index++) {
-//     sum = sum + array[index];
-//     }
-//   return sum;
-// }
+function sum(array) {
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+    sum = sum + array[index];
+    }
+  return sum;
+}
 
-// let sumArray = [1, 2, 3, 4, 5, 6];
-// let calculations = sum(sumArray);
-// console.log(calculations);
+let sumArray = [1, 2, 3, 4, 5, 6];
+let calculations = sum(sumArray);
+console.log(calculations);
 
-// function addition() {
-//     let sum = 0;
-//     for (let index = 0; index < arguments.length; index++) {
-//         sum =sum + arguments[index];
-//     }
-//     return sum;
-// }
+function addition() {
+    let sum = 0;
+    for (let index = 0; index < arguments.length; index++) {
+        sum =sum + arguments[index];
+    }
+    return sum;
+}
 
-// let values = addition(7,8,9,10,11,12,14,15)
-// console.log(values)
+let values = addition(7,8,9,10,11,12,14,15)
+console.log(values)
 
-// Arrow function
-// let arrowFun=()=>{
-//     console.log('this is the arrow function')
-// }
-// arrowFun()
+Arrow function
+let arrowFun=()=>{
+    console.log('this is the arrow function')
+}
+arrowFun()
 
 let arrFun =(val1, val2)=>{
     let addition = val1 + val2
