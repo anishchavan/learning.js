00c7ffb87b3b88fba308a9885b37d64be52478fb
anishchavan <anishchavan29@gmail.com>
2023-05-14 13:05:59 +0530

    various array methods

--- array.js
+++ array.js
@@ -22,6 +22,68 @@
 // console.log(names)
 // console.log(names.slice(2,4))
 
-let fruits=['banana', 'pineapple','orange', 'grapes', 'sapota']
-fruits.splice(2,2,'watermelon', 'guava')
-console.log(fruits)
+// let fruits=['banana', 'pineapple','orange', 'grapes', 'sapota']
+// fruits.splice(2,2,'watermelon', 'guava')
+// console.log(fruits)
+
+// concationations
+
+// let arr1 = [1,2,3,4]
+// let arr2 = [5,6,7,8]
+// let arr3 = [9,10,11,12]
+// console.log(arr1.concat(arr2))
+// console.log(arr1.concat(arr2,arr3))
+
+// includes
+// let myArray = [1,2,3,4,5]
+// console.log(myArray.includes(4,3))
+// console.log(myArray.includes(4,4))
+
+// indexOf
+// console.log(myArray.indexOf(3))
+// console.log(myArray.indexOf(4))
+
+// isArray
+// console.log(Array.isArray(myArray))
+// let str = 'satish'
+// console.log(Array.isArray(str))
+
+// join
+// console.log(myArray.join(' '))
+// console.log(myArray.join(' anish '))
+
+// lastindexOf
+// let we = ['anish','suhas','shyam','mukesh','ganesh','anish']
+// console.log(we.lastIndexOf('anish'))
+
+// map
+// let calc = [1,4,9,16,25]
+// console.log(calc.map(Math.sqrt))
+
+// pop
+// let nums = [1, 2, 3, 4, 5, 6, 7]
+// console.log(nums.pop())
+// console.log(nums) it modifies the original array
+
+//reverse
+// let nums = [1, 2, 3, 4, 5, 6, 7]
+// console.log(nums.reverse())
+
+// shift
+// let nums = [1, 2, 3, 4, 5, 6, 7]
+// console.log(nums.shift())
+
+// sort
+// let we = ['anish','suhas','vhyam','mukesh','ganesh','vanish']
+// console.log(we.sort())
+// console.log(we.reverse())
+// console.log(we.reverse(we.sort())) gives same output like above line
+
+// unShift
+// let stationary = ['pen', 'eraser','notebook','pencil','sharpner']
+// console.log(stationary.unshift('refill','ruler'))
+// console.log(stationary)
+
+// converting string to array
+let myName = 'Anish'
+console.log(myName.split(''))
