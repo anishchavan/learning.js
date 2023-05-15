3f4fdf668c9f5077b7524e00a0e08a4f004804a4
anishchavan <anishchavan29@gmail.com>
2023-05-15 23:06:14 +0530

    this contains notes regarding prototype and inheritance

--- object.js
+++ object.js
@@ -1,10 +1,10 @@
 // object
 
-let userName = {
-  firstName: "Anish",
-  lastName: "Chavan",
-  mobile: 8600236197,
-};
+// let userName = {
+//   firstName: "Anish",
+//   lastName: "Chavan",
+//   mobile: 8600236197,
+// };
 
 // console.log(userName)
 // console.log(userName.lastName)
@@ -12,6 +12,85 @@ let userName = {
 // console.log(userName)
 
 // for in
-for (let x in userName) {
-    console.log(userName[x])
-}
+// for (let x in userName) {
+//     console.log(userName[x])
+// }
+
+// const object = {
+//     rocket: '🚀',
+//     home: '🏡'
+// }
+
+// console.log(object)
+
+// string, number, bigint, boolean, null, undefine, symbol
+
+// const obj1 = {
+//     rocket: '🚀',
+//     fuel: 2,
+//     config: {
+//         name: 'mars'
+//     }
+// }
+
+// obj1.fuel = 200
+// obj1['year'] = 2000
+
+// console.log(obj1)
+
+// part 2
+
+// const obj2 = new Object() //constructor
+// obj2.redbook = '📕'
+// obj2.bluebook = '📘'
+// obj2.myValue = '1 blue book'
+
+// console.log(obj2);
+
+// part 3
+// const powers = {
+//     fly: true,
+//     cordinate: Math.random() + 2
+// }
+
+// const obj3 = Object.create(powers)
+// console.log(obj3);
+// console.log(obj3.cordinate);
+// console.log(Object.getPrototypeOf(obj3));
+
+//part 4
+
+// const obj4 = Object.create({})
+// Object.defineProperty(obj4, 'book', {
+//     icon: '📘',
+//     get: () => '📘',
+//     enumerable: true
+// })
+
+// console.log(obj4);
+// console.log(obj4.book);
+// console.log(Object.getPrototypeOf(obj4));
+
+// for(k in obj4){
+//     console.log("value is: ", k);
+// }
+
+//part 5
+// const obj5 = {
+//     comics : 'marvel',
+//     pen: '',
+    // printComic: function(){
+    //     this.pen += '🖊️🗒️'
+    //     // console.log(this);
+    //     return this;
+    // },
+//     printaComic: () => {
+//        this.pen += '🖊️🗒️'
+//         console.log(this);
+
+//     },
+    
+// }
+
+// console.log(obj5.printComic().printComic().printComic());
+// console.log(obj5.printaComic());
